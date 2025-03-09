9bdd50f0fbd4019cb0e4ad8ee7c844c32ed3db20
lolzio5 <loleziovioramarquet@gmail.com>
2025-03-09 13:03:22 +0000

    moving systate to .cpp file

--- include/globals.h
+++ include/globals.h
@@ -18,24 +18,6 @@ extern volatile uint32_t activeStepSizes[12];//Has one for each key
 
 extern const uint32_t stepSizes[];
 
-//SysState
-typedef struct __sysState_t{
-    std::bitset<32> inputs;
-    int Volume;
-    bool mute = false;
-    bool slave = false;
-    SemaphoreHandle_t mutex;
-    uint8_t RX_Message[8];   
-    int Octave = 0;
-    // 0: Home Screen, with volume and octave control
-    // 1: Menu
-    // 2: DOOM
-    // 3: Wave
-    std::bitset<4> activityList; // Shows the current state of the system. If a bit is set, then the system is currently in that state.
-    bool joystickPress = false;
-    int joystickDirection = 0;
-    } sysState_t;
-
 extern sysState_t sysState;
 
 //CAN Queues
--- src/globals.cpp
+++ src/globals.cpp
@@ -29,7 +29,22 @@ const uint32_t stepSizes[] = { //22kHz between each node
 };
 
 //SysState
-sysState_t sysState;
+typedef struct __sysState_t{
+  std::bitset<32> inputs;
+  int Volume;
+  bool mute = false;
+  bool slave = false;
+  SemaphoreHandle_t mutex;
+  uint8_t RX_Message[8];   
+  int Octave = 0;
+  // 0: Home Screen, with volume and octave control
+  // 1: Menu
+  // 2: DOOM
+  // 3: Wave
+  std::bitset<4> activityList; // Shows the current state of the system. If a bit is set, then the system is currently in that state.
+  bool joystickPress = false;
+  int joystickDirection = 0;
+  } sysState_t;
 
 //CAN Queues
 QueueHandle_t msgInQ;
