368b1244a39733309d3507253cbb309d417e7c5b
lolzio5 <loleziovioramarquet@gmail.com>
2025-03-09 12:57:15 +0000

    managed to successfully merge with menu screen

--- include/globals.h
+++ include/globals.h
@@ -23,13 +23,18 @@ typedef struct __sysState_t{
     std::bitset<32> inputs;
     int Volume;
     bool mute = false;
-    bool slave = false; // SLAVE sends, MASTER receives
+    bool slave = false;
     SemaphoreHandle_t mutex;
     uint8_t RX_Message[8];   
     int Octave = 0;
-    bool doomMode = false;
+    // 0: Home Screen, with volume and octave control
+    // 1: Menu
+    // 2: DOOM
+    // 3: Wave
+    std::bitset<4> activityList; // Shows the current state of the system. If a bit is set, then the system is currently in that state.
     bool joystickPress = false;
-} sysState_t;
+    int joystickDirection = 0;
+    } sysState_t;
 
 extern sysState_t sysState;
 
